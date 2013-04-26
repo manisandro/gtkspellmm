1f50fdac53ff138d9936f6db988c8be264880f1f
Sandro Mani <manisandro@gmail.com>
2013-04-26 13:44:32 +0200

    License as GPLv2.1+

--- codegen/extradefs/generate_extra_defs_gtkspell.cc
+++ codegen/extradefs/generate_extra_defs_gtkspell.cc
@@ -3,17 +3,17 @@
  * This file is part of gtkspellmm.
  *
  * gtkspellmm is free software: you can redistribute it and/or modify it
- * under the terms of the GNU Lesser General Public License as published
- * by the Free Software Foundation, either version 2.1 of the License,
- * or (at your option) any later version.
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation, either version 2.1 of the License, or
+ * (at your option) any later version.
  *
  * gtkspellmm is distributed in the hope that it will be useful, but
  * WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- * See the GNU Lesser General Public License for more details.
+ * See the GNU General Public License for more details.
  *
- * You should have received a copy of the GNU Lesser General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU General Public License along
+ * with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #include <glibmm_generate_extra_defs/generate_extra_defs.h>
